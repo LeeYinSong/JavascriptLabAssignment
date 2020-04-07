020a89ba2d2bcbd156b555e884fd37a0cb496c17
LeeYinSong <61221503+LeeYinSong@users.noreply.github.com>
2020-04-07 16:49:10 +0800

    Add files via upload

---
+++ project.js
@@ -0,0 +1,8 @@
+function burgerMenu() {
+  let x = document.getElementById("burgerMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
