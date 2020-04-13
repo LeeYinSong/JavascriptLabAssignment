202ec96af17e90ab500a192c303292d146a8c317
LeeYinSong <61221503+LeeYinSong@users.noreply.github.com>
2020-04-13 15:15:03 +0800

    Delete project.js

--- project.js
+++
@@ -1,8 +0,0 @@
-function burgerMenu() {
-  let x = document.getElementById("burgerMenu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
